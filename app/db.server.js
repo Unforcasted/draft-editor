2c845a764a6ae4d551916cecca8e90a9a7e0b6f1
Vyacheslav Goncharuk <vyacheslav.goncharuk@miracommerce.com>
2025-04-20 21:45:45 +0200

    prisma test

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import PrismaPkg from "@prisma/client";
+const { PrismaClient } = PrismaPkg;
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
   }
 }
-
 const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
