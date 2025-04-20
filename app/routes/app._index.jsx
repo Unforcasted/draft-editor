cd17c4d69c3a5c53acc61277308f2bdefe11593c
Vyacheslav Goncharuk <vyacheslav.goncharuk@miracommerce.com>
2025-04-20 22:21:17 +0200

    sorted drafts + pagination

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -12,12 +12,14 @@ import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   const { admin } = await authenticate.admin(request);
+  const url = new URL(request.url);
+  const after = url.searchParams.get("after") || null;
 
-  const response = await admin.graphql(
-    `#graphql
-    query {
-      draftOrders(first: 10) {
+  const query = `#graphql
+    query draftOrders($after: String) {
+      draftOrders(first: 10, after: $after, reverse: true, sortKey: ID) {
         edges {
+          cursor
           node {
             id
             name
@@ -26,27 +28,50 @@ export const loader = async ({ request }) => {
             invoiceUrl
           }
         }
+        pageInfo {
+          hasNextPage
+        }
       }
-    }`
-  );
+    }`;
+
+  const response = await admin.graphql(query, {
+    variables: { after },
+  });
 
   const data = await response.json();
 
-  const draftOrders = data.data.draftOrders.edges.map(edge => edge.node);
+  const edges = data.data.draftOrders.edges;
+  const draftOrders = edges.map(edge => edge.node);
+  const lastCursor = edges.length > 0 ? edges[edges.length - 1].cursor : null;
+  const hasNextPage = data.data.draftOrders.pageInfo.hasNextPage;
 
-  return json({ draftOrders });
+  return json({ draftOrders, lastCursor, hasNextPage });
 };
 
 export default function DraftOrdersPage() {
-  const { draftOrders } = useLoaderData();
+  const { draftOrders, lastCursor, hasNextPage } = useLoaderData();
 
   const rows = draftOrders.map(order => [
     order.name ? (
-        <Link url={`/app/draft?id=${encodeURIComponent(order.id.replace('gid://shopify/DraftOrder/', ''))}`}>{order.name}</Link>
-    ) : ("#"),
-    
+      <Link
+        url={`/app/draft?id=${encodeURIComponent(
+          order.id.replace("gid://shopify/DraftOrder/", "")
+        )}`}
+      >
+        {order.name}
+      </Link>
+    ) : (
+      "#"
+    ),
     order.status,
     new Date(order.createdAt).toLocaleDateString(),
+    order.invoiceUrl ? (
+      <Link url={order.invoiceUrl} target="_blank">
+        Invoice
+      </Link>
+    ) : (
+      "-"
+    ),
   ]);
 
   return (
@@ -61,6 +86,16 @@ export default function DraftOrdersPage() {
             headings={["Order", "Status", "Created At", "Invoice"]}
             rows={rows}
           />
+          {hasNextPage && (
+            <RemixLink
+              to={`?after=${encodeURIComponent(lastCursor)}`}
+              style={{ alignSelf: "flex-start", marginTop: "16px" }}
+            >
+              <Text as="span" variant="bodyMd">
+                ➕ Load more
+              </Text>
+            </RemixLink>
+          )}
         </BlockStack>
       </Card>
     </Page>
